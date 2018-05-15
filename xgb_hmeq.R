65bdc846ed69ae82030811636d6efae8d5343f57
Nguyen Chi Dung <nguyenchidung.neu@gmail.com>
2018-05-15 22:13:31 +0700

    Add files via upload

--- xgb_hmeq.R
+++ xgb_hmeq.R
@@ -99,38 +99,22 @@ xgb_Tree2 <- train(BAD ~ .,
                    tuneLength = 5, 
                    trControl = train.control)
 
+# Tham số tối ưu: 
 xgb_Tree2$bestTune 
-
-# So sánh 14 tiêu chí đánh giá chất lượng mô hình phân loại
-# và thấy rằng XGB2 - tức tối ưu hóa tham số theo AUC là tốt hơn: 
-df_results <- bind_rows(xgb_Tree1$resample %>% mutate(Model = "XGB1"), 
-                        xgb_Tree2$resample %>% mutate(Model = "XGB2"))
-
-
-theme_set(theme_minimal())
-df_results %>% 
-  select(-Resample) %>% 
-  gather(a, b, -Model) %>% 
-  ggplot(aes(Model, b, color = Model, fill = Model)) + 
-  geom_boxplot(alpha = 0.3, show.legend = FALSE) + 
-  facet_wrap(~ a, scales = "free") + 
-  labs(x = NULL, y = NULL)
-
 
 # Thiết lập một loạt các tham số tinh chỉnh của xbgTree : 
 
-
 tune_grid <- expand.grid(
   nrounds = seq(200, 300, by = 50), 
-  max_depth = 4:6,
-  eta = seq(0.2, 0.5, by = 0.1),
-  gamma = seq(0, 0.1, by = 0.025), 
-  colsample_bytree = seq(0.5, 0.8, by = 0.1), 
-  min_child_weight = seq(1, 3, by = 1), 
-  subsample = 1:3)
+  max_depth = 4:7,
+  eta = 0.3,
+  gamma = 0, 
+  colsample_bytree = 0.6, 
+  min_child_weight = 1, 
+  subsample = seq(0.1, 0.9, by = 0.1))
 
 
-# có lẽ cần 24h để chạy mô hình: 
+# Chạy XGB với lưới tham số tinh chỉnh ở trên: 
 set.seed(1)
 xgb_Tree3 <- train(BAD ~ ., 
                    data = train_df, 
@@ -139,10 +123,108 @@ xgb_Tree3 <- train(BAD ~ .,
                    tuneGrid = tune_grid, 
                    trControl = train.control)
 
-xgb_Tree3
+# Tham số tối ưu: 
+xgb_Tree3$bestTune
+
+# So sánh 14 tiêu chí đánh giá chất lượng mô hình phân loại
+# và thấy rằng XGB3 là tốt nhất nếu căn cứ vào Accuracy: 
+
+df_results <- bind_rows(xgb_Tree1$resample %>% mutate(Model = "XGB1"), 
+                        xgb_Tree2$resample %>% mutate(Model = "XGB2"), 
+                        xgb_Tree3$resample %>% mutate(Model = "XGB3"))
 
 
+theme_set(theme_minimal())
 
+df_results %>% 
+  select(-Resample) %>% 
+  gather(a, b, -Model) %>% 
+  ggplot(aes(Model, b, color = Model, fill = Model)) + 
+  geom_boxplot(alpha = 0.3, show.legend = FALSE) + 
+  facet_wrap(~ a, scales = "free") + 
+  labs(x = NULL, y = NULL)
+
+# Tính toán cụ thể: 
+df_results %>% 
+  group_by(Model) %>% 
+  summarise_each(funs(median, mean, min, max, n()), Accuracy) %>% 
+  arrange(-Accuracy_median)
+
+
+df_results %>% 
+  group_by(Model) %>% 
+  summarise_each(funs(median, mean, min, max, n()), AUC) %>% 
+  arrange(-AUC_median)
+
+
+# Viết hàm tính CM: 
+
+my_cm <- function(model, test_df) {
+  pred <- predict(model, test_df)
+  confusionMatrix(test_df$BAD, pred)
+}
+
+
+# Có dấu hiệu cho thấy XGB2 là tốt nhất nếu mục tiêu là vì lợi nhuận: 
+my_cm(xgb_Tree1, test_df = test_df %>% group_by(BAD) %>% sample_n(500) %>% ungroup())
+my_cm(xgb_Tree2, test_df = test_df %>% group_by(BAD) %>% sample_n(500) %>% ungroup())
+my_cm(xgb_Tree3, test_df = test_df %>% group_by(BAD) %>% sample_n(500) %>% ungroup())
+
+# Viết hàm tính BG - GB: 
+
+my_results <- function(model, input_df) {
+  pred <- predict(model, input_df)
+  m <- confusionMatrix(input_df$BAD, pred)
+  m$table %>% 
+    as.vector() %>% 
+    matrix(ncol = 4) %>% 
+    as.data.frame() %>% 
+    rename(BB = V1, GB = V2, BG = V3, GG = V4) %>% 
+    return()
+  
+}
+
+# Cho XGB1 đến XGB3: 
+
+lapply(1:1000, function(i) {
+  set.seed(i)
+  my_results(xgb_Tree1,  test_df %>% group_by(BAD) %>% sample_n(500) %>% ungroup())
+  
+}) -> m1
+
+lapply(1:1000, function(i) {
+  set.seed(i)
+  my_results(xgb_Tree2,  test_df %>% group_by(BAD) %>% sample_n(500) %>% ungroup())
+  
+}) -> m2
+
+lapply(1:1000, function(i) {
+  set.seed(i)
+  my_results(xgb_Tree3,  test_df %>% group_by(BAD) %>% sample_n(500) %>% ungroup())
+  
+}) -> m3
+
+
+# Hàm chuyển hóa về DF: 
+convert_to_df <- function(x) {
+  do.call("bind_rows", x)
+}
+
+
+# Sử dụng hàm: 
+df1 <- convert_to_df(m1) %>% mutate(Model = "XGB1")
+df2 <- convert_to_df(m2) %>% mutate(Model = "XGB2")
+df3 <- convert_to_df(m3) %>% mutate(Model = "XGB3")
+
+# Xếp nhầm hồ sơ Xấu thành tốt thì XGB2 thấp nhất: 
+df1 %>% pull(BG) %>% summary()
+df2 %>% pull(BG) %>% summary()
+df3 %>% pull(BG) %>% summary()
+
+# XGB3 xếp đúng tốt thành tốt cao nhất: 
+df1 %>% pull(GG) %>% summary()
+df2 %>% pull(GG) %>% summary()
+df3 %>% pull(GG) %>% summary()
 
 
 
